31e329887a016176b71abf48df0a93c893c29994
Jon Gordon <jon@jongor-software.com>
2015-05-12 21:22:19 +0100

    Initial Commit

---
+++ Colour MakerTests/Colour_MakerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Colour_MakerTests.swift
+//  Colour MakerTests
+//
+//  Created by Jon Gordon on 12/05/2015.
+//  Copyright (c) 2015 JonGor Software. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Colour_MakerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
